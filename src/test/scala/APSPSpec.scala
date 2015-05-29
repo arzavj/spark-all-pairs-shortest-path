1d4af67bfacddc60664edc363f859dec2fe9461f
Arzav Jain <arzavj@stanford.edu>
2015-05-28 22:11:31 -0700

    wrote a test but equality of breeze matrices is not working

--- src/test/scala/APSPSpec.scala
+++ src/test/scala/APSPSpec.scala
@@ -1,10 +1,9 @@
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry}
 import org.scalatest.{FlatSpec}
+import AllPairsShortestPath._
+import breeze.linalg.{DenseMatrix => BDM}
 
-/**
- * Created by arzav on 5/26/15.
- */
 class APSPSpec extends FlatSpec {
 
   def fixture = {
@@ -22,11 +21,23 @@ class APSPSpec extends FlatSpec {
       (2, 1, 6), (2, 3, 5), (3, 0, 4), (3, 1, 2), (3, 2, 2))).map { case (i, j, v) => MatrixEntry(i, j, v) }
     val coordMat = new CoordinateMatrix(entries)
     val matA = coordMat.toBlockMatrix(2, 2).cache()
-    val ApspPartitioner = GridPartitioner(matA.numRowBlocks, matA.numColBlocks, matA.blocks.partitions.length)
     matA
+  }
+
+  def ApspPartitioner = {
+    GridPartitioner(fourByFourBlockMatrx.numRowBlocks, fourByFourBlockMatrx.numColBlocks, fourByFourBlockMatrx.blocks.partitions.length)
   }
 
   "The sample 4x4 Block Matrix" should "be valid" in {
     fourByFourBlockMatrx.validate()
+  }
+
+  it should "match our APSP matrix" in {
+    assert(toBreeze(distributedApsp(fourByFourBlockMatrx, 1, ApspPartitioner).toLocalMatrix()) === BDM(
+      (0.0, 4.0, 4.0, 2.0),
+      (2.0, 0.0, 1.0, 3.0),
+      (1.0, 5.0, 0.0, 3.0),
+      (3.0, 2.0, 2.0, 0.0)
+    ))
   }
 }
